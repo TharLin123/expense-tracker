967ec2f03756ac85b799e381e2a0a919e102895f
Yuchen Xin <e0838400@u.nus.edu>
2021-12-22 19:42:07 +0800

    fix the empty validator class

--- secondleave/src/main/java/sg/nus/edu/secondleave/validators/LeaveFormValidator.java
+++ secondleave/src/main/java/sg/nus/edu/secondleave/validators/LeaveFormValidator.java
@@ -1,5 +1,33 @@
 package sg.nus.edu.secondleave.validators;
 
-public class LeaveFormValidator {
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+
+import sg.nus.edu.secondleave.model.LeaveApplication;
+
+@Component
+public class LeaveFormValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> arg0) {
+		// TODO Auto-generated method stub
+		return LeaveApplication.class.isAssignableFrom(arg0);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+		LeaveApplication leave = (LeaveApplication) target;
+		if ((leave.getFromDate()!=null && leave.getToDate()!=null)&&(leave.getFromDate().compareTo(leave.getToDate()) > 0)) {
+			errors.reject("toDate", "Leave end date should be greater than leave start date.");
+			errors.rejectValue("toDate", "error.dates", "to date must be > from date");	
+		}
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "reason", "error.reason", "Reason is required.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "fromDate", "error.fromDate", "From Date is required.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "toDate", "error.toDate", "To Date is required.");
+	}
 
 }
